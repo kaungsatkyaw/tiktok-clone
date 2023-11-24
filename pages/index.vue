0c8f3ee5f0ef86009f21bdceb92ff3741e3c2186
Kaung Sat Kyaw <kaungsatkyaw@cgm-myanmar.com>
2023-11-24 14:40:20 +0630

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <MainLayout>
+    <div class="pt-[80px] w-[calc(100%-90px)] max-w-[690px]">
+      <!-- <div v-for="post in $generalStore.posts" :key="post"> -->
+        <PostMain />
+      <!-- </div> -->
+    </div>
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '../layouts/MainLayout.vue';
+
+</script>
